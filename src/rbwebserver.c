19d2b325a079cd048d51fd4c3f839725b28be866
Vojtěch Boček <vojtech.bocek@avast.com>
2020-02-13 20:07:34 +0100

    Use built-in httpd server from ESP-IDF

--- src/rbwebserver.c
+++ src/rbwebserver.c
@@ -15,350 +15,85 @@
 #include "freertos/task.h"
 #include "esp_log.h"
 #include "esp_spiffs.h"
+#include <esp_http_server.h>
 
 #define TAG "RbWebServer"
 
 #define WORKING_DIRECTORY "/spiffs"
 
-#define LISTENQ  64  /* second argument to listen() */
-#define MAXLINE 256   /* max length of a line */
-#define RIO_BUFSIZE 256
-#define FILENAME_SIZE 64
+static esp_err_t static_file_handler(httpd_req_t *req) {
+    char buf[256] = { 0 };
 
-typedef struct {
-    int rio_fd;                 /* descriptor for this buf */
-    int rio_cnt;                /* unread byte in this buf */
-    char *rio_bufptr;           /* next unread byte in this buf */
-    char rio_buf[RIO_BUFSIZE];  /* internal buffer */
-} rio_t;
-
-/* Simplifies calls to bind(), connect(), and accept() */
-typedef struct sockaddr SA;
-
-typedef struct {
-    char filename[FILENAME_SIZE];
-    off_t offset;              /* for support Range */
-    size_t end;
-} http_request;
-
-typedef struct {
-    const char *extension;
-    const char *mime_type;
-} mime_map;
-
-mime_map meme_types [] = {
-    {".css", "text/css"},
-    {".gif", "image/gif"},
-    {".htm", "text/html"},
-    {".html", "text/html"},
-    {".jpeg", "image/jpeg"},
-    {".jpg", "image/jpeg"},
-    {".ico", "image/x-icon"},
-    {".js", "application/javascript"},
-    {".pdf", "application/pdf"},
-    {".mp4", "video/mp4"},
-    {".png", "image/png"},
-    {".svg", "image/svg+xml"},
-    {".xml", "text/xml"},
-    {NULL, NULL},
-};
-
-char *default_mime_type = "text/plain";
-
-void rio_readinitb(rio_t *rp, int fd){
-    rp->rio_fd = fd;
-    rp->rio_cnt = 0;
-    rp->rio_bufptr = rp->rio_buf;
-}
-
-ssize_t writen(int fd, void *usrbuf, size_t n){
-    size_t nleft = n;
-    ssize_t nwritten;
-    char *bufp = usrbuf;
-
-    while (nleft > 0){
-        if ((nwritten = write(fd, bufp, nleft)) <= 0){
-            if (errno == EINTR)  /* interrupted by sig handler return */
-                nwritten = 0;    /* and call write() again */
-            else
-                return -1;       /* errorno set by write() */
-        }
-        nleft -= nwritten;
-        bufp += nwritten;
-    }
-    return n;
-}
-
-
-/*
- * rio_read - This is a wrapper for the Unix read() function that
- *    transfers min(n, rio_cnt) bytes from an internal buffer to a user
- *    buffer, where n is the number of bytes requested by the user and
- *    rio_cnt is the number of unread bytes in the internal buffer. On
- *    entry, rio_read() refills the internal buffer via a call to
- *    read() if the internal buffer is empty.
- */
-/* $begin rio_read */
-static ssize_t rio_read(rio_t *rp, char *usrbuf, size_t n){
-    int cnt;
-    while (rp->rio_cnt <= 0){  /* refill if buf is empty */
-
-        rp->rio_cnt = read(rp->rio_fd, rp->rio_buf,
-                           sizeof(rp->rio_buf));
-        if (rp->rio_cnt < 0){
-            if (errno != EINTR) /* interrupted by sig handler return */
-                return -1;
-        }
-        else if (rp->rio_cnt == 0)  /* EOF */
-            return 0;
-        else
-            rp->rio_bufptr = rp->rio_buf; /* reset buffer ptr */
+    if(snprintf(buf, sizeof(buf), "%s%s", WORKING_DIRECTORY, req->uri) >= sizeof(buf)) {
+        ESP_LOGE(TAG, "the uri %s is too long!", req->uri);
+        return ESP_ERR_NOT_SUPPORTED;
     }
 
-    /* Copy min(n, rp->rio_cnt) bytes from internal buf to user buf */
-    cnt = n;
-    if (rp->rio_cnt < n)
-        cnt = rp->rio_cnt;
-    memcpy(usrbuf, rp->rio_bufptr, cnt);
-    rp->rio_bufptr += cnt;
-    rp->rio_cnt -= cnt;
-    return cnt;
+    int fd = open(buf, O_RDONLY);
+    if(fd <= 0) {
+        return ESP_ERR_NOT_FOUND;
+    }
+
+    int n = 0;
+    esp_err_t err = ESP_OK;
+    while((n = read(fd, buf, sizeof(buf))) > 0) {
+        if((err = httpd_resp_send_chunk(req, buf, n)) != ESP_OK)
+            goto exit;
+    }
+    err = httpd_resp_send_chunk(req, buf, 0);
+
+exit:
+    close(fd);
+    return err;
 }
 
-/*
- * rio_readlineb - robustly read a text line (buffered)
- */
-ssize_t rio_readlineb(rio_t *rp, void *usrbuf, size_t maxlen){
-    int n, rc;
-    char c, *bufp = usrbuf;
+static void add_handlers(httpd_handle_t server, const char *path) {
+    DIR *d = opendir(path);
+    if(d == NULL) {
+        ESP_LOGE(TAG, "failed to open %s\n", path);
+        return;
+    }
 
-    for (n = 1; n < maxlen; n++){
-        if ((rc = rio_read(rp, &c, 1)) == 1){
-            *bufp++ = c;
-            if (c == '\n')
+    char buf[256] = { 0 };
+    struct dirent *e = NULL;
+    while((e = readdir(d)) != NULL) {
+        if(snprintf(buf, sizeof(buf), "%s/%s", path, e->d_name) >= sizeof(buf)) {
+            ESP_LOGE(TAG, "too long filename: %s/%s", path, e->d_name);
+            continue;
+        }
+
+        switch(e->d_type) {
+            case DT_DIR:
+                add_handlers(server, buf);
                 break;
-        } else if (rc == 0){
-            if (n == 1)
-                return 0; /* EOF, no data read */
-            else
-                break;    /* EOF, some data was read */
-        } else
-            return -1;    /* error */
-    }
-    *bufp = 0;
-    return n;
-}
+            case DT_REG:
+            {
+                httpd_uri_t handler = {
+                    .uri = buf+strlen(WORKING_DIRECTORY),
+                    .method = HTTP_GET,
+                    .handler = static_file_handler,
+                };
 
-static const char* get_mime_type(char *filename){
-    char *dot = strrchr(filename, '.');
-    if(dot){ // strrchar Locate last occurrence of character in string
-        mime_map *map = meme_types;
-        while(map->extension){
-            if(strcmp(map->extension, dot) == 0){
-                return map->mime_type;
-            }
-            map++;
-        }
-    }
-    return default_mime_type;
-}
-
-
-int open_listenfd(int port){
-    int listenfd, optval=1;
-    struct sockaddr_in serveraddr;
-
-    /* Create a socket descriptor */
-    if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-        return -1;
-
-    /* Eliminates "Address already in use" error from bind. */
-    if (setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR,
-                   (const void *)&optval , sizeof(int)) < 0)
-        return -1;
-
-    /* Listenfd will be an endpoint for all requests to port
-       on any IP address for this host */
-    memset(&serveraddr, 0, sizeof(serveraddr));
-    serveraddr.sin_family = AF_INET;
-    serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    serveraddr.sin_port = htons((unsigned short)port);
-    if (bind(listenfd, (SA *)&serveraddr, sizeof(serveraddr)) < 0)
-        return -1;
-
-    /* Make it a listening socket ready to accept connection requests */
-    if (listen(listenfd, LISTENQ) < 0)
-        return -1;
-    return listenfd;
-}
-
-void url_decode(char* src, char* dest, int max) {
-    char *p = src;
-
-    int prefix_len = snprintf(dest, FILENAME_SIZE, "%s/", WORKING_DIRECTORY);
-    dest += prefix_len;
-
-    char code[3] = { 0 };
-    while(*p && --max) {
-        if(*p == '%') {
-            memcpy(code, ++p, 2);
-            *dest++ = (char)strtoul(code, NULL, 16);
-            p += 2;
-        } else {
-            *dest++ = *p++;
-        }
-    }
-    *dest = '\0';
-}
-
-void parse_request(int fd, http_request *req){
-    rio_t rio;
-    char buf[MAXLINE], method[MAXLINE], uri[MAXLINE];
-    req->offset = 0;
-    req->end = 0;              /* default */
-
-    rio_readinitb(&rio, fd);
-    rio_readlineb(&rio, buf, MAXLINE);
-    sscanf(buf, "%s %s", method, uri); /* version is not cared */
-    /* read all */
-    while(buf[0] != '\n' && buf[1] != '\n') { /* \n || \r\n */
-        rio_readlineb(&rio, buf, MAXLINE);
-        if(buf[0] == 'R' && buf[1] == 'a' && buf[2] == 'n'){
-            sscanf(buf, "Range: bytes=%ld-%u", &req->offset, &req->end);
-            // Range: [start, end]
-            if( req->end != 0) req->end ++;
-        }
-    }
-    char* filename = uri;
-    if(uri[0] == '/'){
-        filename = uri + 1;
-        int length = strlen(filename);
-        if (length == 0){
-            filename = ".";
-        } else {
-            for (int i = 0; i < length; ++ i) {
-                if (filename[i] == '?') {
-                    filename[i] = '\0';
-                    break;
+                esp_err_t err = httpd_register_uri_handler(server, &handler);
+                if(err != ESP_OK) {
+                    ESP_LOGE(TAG, "failed to register uri handler for %s: %d\n", buf, err);
                 }
+                break;
             }
         }
     }
-    url_decode(filename, req->filename, MAXLINE);
+
+    closedir(d);
 }
 
-void log_access(int status, struct sockaddr_in *c_addr, http_request *req){
-    ESP_LOGI(TAG, "%s:%d %d - %s", inet_ntoa(c_addr->sin_addr),
-           ntohs(c_addr->sin_port), status, req->filename);
-}
-
-void client_error(int fd, int status, char *msg, char *longmsg){
-    char buf[MAXLINE];
-    sprintf(buf, "HTTP/1.1 %d %s\r\n", status, msg);
-    sprintf(buf + strlen(buf),
-            "Content-length: %u\r\n\r\n", strlen(longmsg));
-    sprintf(buf + strlen(buf), "%s", longmsg);
-    writen(fd, buf, strlen(buf));
-}
-
-static ssize_t sendfile(int out_fd, int in_fd, off_t *offset, size_t count) {
-    char buf[256];
-    size_t chunk;
-    ssize_t res;
-    ssize_t chunk_res;
-    while(count > 0) {
-        chunk = count;
-        if(chunk > sizeof(buf))
-            chunk = sizeof(buf);
-
-        chunk_res = read(in_fd, buf, chunk);
-        if(chunk_res < 0) {
-            return -1;
-        }
-
-        chunk_res = write(out_fd, buf, chunk_res);
-        if(chunk_res < 0) {
-            return -1;
-        }
-        res += chunk_res;
-        count -= chunk;
-    }
-    return res;
-}
-
-void serve_static(int out_fd, int in_fd, http_request *req,
-                  size_t total_size){
-    char buf[256];
-    if (req->offset > 0){
-        sprintf(buf, "HTTP/1.1 206 Partial\r\n");
-        sprintf(buf + strlen(buf), "Content-Range: bytes %ld-%u/%u\r\n",
-                req->offset, req->end, total_size);
-    } else {
-        sprintf(buf, "HTTP/1.1 200 OK\r\nAccept-Ranges: bytes\r\n");
-    }
-    sprintf(buf + strlen(buf), "Cache-Control: no-cache\r\n");
-    // sprintf(buf + strlen(buf), "Cache-Control: public, max-age=315360000\r\nExpires: Thu, 31 Dec 2037 23:55:55 GMT\r\n");
-
-    sprintf(buf + strlen(buf), "Content-length: %lu\r\n",
-            req->end - req->offset);
-    sprintf(buf + strlen(buf), "Content-type: %s\r\n\r\n",
-            get_mime_type(req->filename));
-
-    writen(out_fd, buf, strlen(buf));
-    off_t offset = req->offset; /* copy */
-    while(offset < req->end){
-        if(sendfile(out_fd, in_fd, &offset, req->end - req->offset) <= 0) {
-            break;
-        }
-        ESP_LOGD(TAG, "offset: %ld \n\n", offset);
-        close(out_fd);
-        break;
-    }
-}
-
-void process(int fd, struct sockaddr_in *clientaddr){
-    ESP_LOGD(TAG, "accept request, fd is %d\n", fd);
-    http_request req;
-    parse_request(fd, &req);
-
-    struct stat sbuf;
-    int status = 200, ffd = open(req.filename, O_RDONLY, 0);
-    if(ffd <= 0){
-        status = 404;
-        char *msg = "File not found";
-        client_error(fd, status, "Not found", msg);
-    } else {
-        fstat(ffd, &sbuf);
-        if(S_ISREG(sbuf.st_mode)){
-            if (req.end == 0){
-                req.end = sbuf.st_size;
-            }
-            if (req.offset > 0){
-                status = 206;
-            }
-            serve_static(fd, ffd, &req, sbuf.st_size);
-        } else {
-            status = 400;
-            char *msg = "Unknow Error";
-            client_error(fd, status, "Error", msg);
-        }
-        close(ffd);
-    }
-    log_access(status, clientaddr, &req);
-}
-
-static void tiny_web_task(void *portPtr) {
+static void *new_web_task(void *portPtr) {
     int port = (int)portPtr;
-
-    struct sockaddr_in clientaddr;
-    int listenfd, connfd;
-    socklen_t clientlen = sizeof clientaddr;
 
     esp_vfs_spiffs_conf_t conf = {
       .base_path = WORKING_DIRECTORY,
       .partition_label = NULL,
       .max_files = 5,
-      .format_if_mount_failed = true
+      .format_if_mount_failed = true,
     };
 
     esp_err_t ret = esp_vfs_spiffs_register(&conf);
@@ -370,39 +105,30 @@ static void tiny_web_task(void *portPtr) {
         } else {
             ESP_LOGE(TAG, "Failed to initialize SPIFFS (%d)", ret);
         }
-        return;
+        goto exit;
     }
 
-    listenfd = open_listenfd(port);
-    if (listenfd > 0) {
-        ESP_LOGI(TAG, "Listening on port %d", port);
-    } else {
-        ESP_LOGE(TAG, "failed to start: %s", strerror(errno));
-        return;
+    httpd_handle_t server = NULL;
+    httpd_config_t config = HTTPD_DEFAULT_CONFIG();
+    config.server_port = port;
+    config.task_priority = 3;
+    config.max_uri_handlers = 32;
+
+    esp_err_t err = httpd_start(&server, &config);
+    if(err != ESP_OK) {
+        ESP_LOGE(TAG, "failed to start httpd server: %d", err);
+        goto exit;
     }
 
-    if(fcntl(listenfd, F_SETFL, O_NONBLOCK) == -1) {
-        ESP_LOGE(TAG, "failed to set non-blocking: %s", strerror(errno));
-        close(listenfd);
-        return;
-    }
+    add_handlers(server, WORKING_DIRECTORY);
 
-    while(1) {
-        connfd = accept(listenfd, (SA *)&clientaddr, &clientlen);
-        if(connfd >= 0) {
-            process(connfd, &clientaddr);
-            close(connfd);
-        } else if(errno != EWOULDBLOCK && errno != EAGAIN) {
-            ESP_LOGE(TAG, "failed to accept: %s", strerror(errno));
-        }
-
-        vTaskDelay(50 / portTICK_PERIOD_MS);
-    }
+exit:
+    vTaskDelete(NULL);
+    return NULL;
 }
 
 TaskHandle_t rb_web_start(int port) {
     TaskHandle_t task;
-    xTaskCreate(&tiny_web_task, "rbctrl_web", 3072, (void*)port, 3, &task);
+    xTaskCreate(&new_web_task, "rbctrl_web", 3072, (void*)port, 3, &task);
     return task;
 }
-
