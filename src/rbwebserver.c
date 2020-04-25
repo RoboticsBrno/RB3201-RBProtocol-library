45754d95426982de9989b4ff9f57c49fd9825785
Vojtech Bocek <vbocek@gmail.com>
2020-04-25 21:20:36 +0200

    Add .json mime type, remove some unused mime types

--- src/rbwebserver.c
+++ src/rbwebserver.c
@@ -49,18 +49,13 @@ typedef struct {
 
 mime_map meme_types[] = {
     { ".css", "text/css" },
-    { ".gif", "image/gif" },
-    { ".htm", "text/html" },
     { ".html", "text/html" },
     { ".jpeg", "image/jpeg" },
     { ".jpg", "image/jpeg" },
     { ".ico", "image/x-icon" },
     { ".js", "application/javascript" },
-    { ".pdf", "application/pdf" },
-    { ".mp4", "video/mp4" },
     { ".png", "image/png" },
-    { ".svg", "image/svg+xml" },
-    { ".xml", "text/xml" },
+    { ".json", "application/json" },
     { NULL, NULL },
 };
 
