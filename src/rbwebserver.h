2f63e2c5c8e1a70f06504a9fe05d4b9daef9d2ae
Vojtech Bocek <vbocek@gmail.com>
2024-05-28 19:46:33 +0200

    feat: add webserver not found callback

--- src/rbwebserver.h
+++ src/rbwebserver.h
@@ -15,6 +15,11 @@ extern "C" {
 TaskHandle_t rb_web_start(int port);
 
 /**
+ * \brief Start serving files without mounting the spiffs.
+ */
+TaskHandle_t rb_web_start_no_spiffs(int port, const char *working_directory_path);
+
+/**
  * \brief Stop http server. Pass in the task handle returned by rb_web_start.
  */
 void rb_web_stop(TaskHandle_t web_task);
@@ -33,6 +38,21 @@ const char *rb_web_get_files_root(void);
  * Set a callback to call whenever URL in directory /extra/ is accessed.
 */
 void rb_web_set_extra_callback(void (*callback)(const char* request_path, int out_fd));
+
+typedef struct {
+    const uint8_t *data;
+    size_t size;
+    uint8_t is_gzipped;
+} not_found_response_t;
+
+/**
+ * Set a callback to call whenever a 404 would have been returned.
+ * Return zero-filled not_found_response_t to continue with 404, otherwise fill in the fields.
+ * 
+*/
+void rb_web_set_not_found_callback(not_found_response_t (*callback)(const char* request_path));
+
+
 
 #ifdef __cplusplus
 };
